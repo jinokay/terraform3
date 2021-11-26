4fb87a91e3ffd7101a730a87b6cfb204f479ddb3
jinokay <kimjoe1997@naver.com>
2021-11-26 10:46:19 +0900

    step2

--- 10_ec2.tf
+++ 10_ec2.tf
@@ -16,13 +16,13 @@ data "aws_ami" "amzn" {
 
 
 resource "aws_instance" "wook_web" {
-  ami = data.aws_ami.amzn.id
-  instance_type = "t2.micro"
-  key_name = "wook1-key"
+  ami                    = data.aws_ami.amzn.id
+  instance_type          = "t2.micro"
+  key_name               = "wook1-key"
   vpc_security_group_ids = [aws_security_group.wook_sg.id]
-  availability_zone = "ap-northeast-2a"
-  private_ip = "10.0.0.11"
-  subnet_id = aws_subnet.wook_puba.id
+  availability_zone      = "ap-northeast-2a"
+  private_ip             = "10.0.0.11"
+  subnet_id              = aws_subnet.wook_puba.id
 
   tags = {
     "Name" = "wook-web"
@@ -30,10 +30,10 @@ resource "aws_instance" "wook_web" {
 }
 
 resource "aws_eip" "wook_web_ip" {
-  vpc = true
-  instance = aws_instance.wook_web.id
+  vpc                       = true
+  instance                  = aws_instance.wook_web.id
   associate_with_private_ip = "10.0.0.11"
-  depends_on = [aws_internet_gateway.wook_ig]
+  depends_on                = [aws_internet_gateway.wook_ig]
 }
 
 output "public_ip" {
