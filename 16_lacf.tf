0286a6ca8ad020db0ab311f324648fed3f10b3dc
jinokay <kimjoe1997@naver.com>
2021-11-26 15:39:34 +0900

    step1

---
+++ 16_lacf.tf
@@ -0,0 +1,13 @@
+resource "aws_launch_configuration" "wook_lach" {
+  name                 = "wook-lucf"
+  image_id             = aws_ami_from_instance.wook_ami.id
+  instance_type        = "t2.micro"
+  iam_instance_profile = "admin_role"
+  security_groups      = [aws_security_group.wook_sg.id]
+  key_name             = "wook1-key"
+  user_data            = <<-EOF
+            #!/bin/bash
+            systemctl start httpd
+            systemctl enable httpd
+            EOF
+}
